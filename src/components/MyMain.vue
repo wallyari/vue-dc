9c97dc6c6ada7bc59167954bbde1017f1e0f81d4
ariannadepasquale <depasqualearianna@gmail.com>
2022-08-25 17:24:21 +0200

    Add Main Section

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -3,6 +3,30 @@
     <div class="text-white px-3 p-5">
         <h2>--> Content goes here-- </h2>
     </div>
+    <section>
+        <div class="shopper d-flex p-5 text-white">
+            <div class="pe-5">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                DIGITAL COMICS
+            </div>
+            <div class="pe-5">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                DC MERCHANDISE
+            </div>
+            <div class="pe-5">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                COMIC SHOP LOCATOR
+            </div>
+            <div class="pe-5">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                SUBSCRIPTIONS
+            </div>
+            <div class="pe-5">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                DC POWER VISA
+            </div>
+        </div>
+    </section>
 </main>
 
 </template>
@@ -14,6 +38,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+.shopper{
+    background-color: $colorBrand;
+    img{
+        width: 60px;
+        height: 60px;
+        object-fit: contain;
+
+    }
+}
 
 </style> 
