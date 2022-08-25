56ae0dabb6693c7ad74ccfe03488d3b227aaebf9
ariannadepasquale <depasqualearianna@gmail.com>
2022-08-25 15:35:16 +0200

    Add Styles folder

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import '~bootstrap/scss/bootstrap.scss'
+@import '~bootstrap/scss/bootstrap.scss';
+@import './styles/general.scss';
+@import './styles/variables.scss'
+
 
 </style>
